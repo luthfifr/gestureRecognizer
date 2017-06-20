41534ba8fb1ad0f24953860cf79086ac19e4e5e9
Luthfi Fathur Rahman <LuthfiFR@LFR-MacBook-Air.local>
2017-06-20 15:40:33 +0700

    edit the UISwipeGestureRecognizer codes and 2 images of card

--- gestureRecognizers/SwipeVC.swift
+++ gestureRecognizers/SwipeVC.swift
@@ -10,13 +10,30 @@ import UIKit
 
 class SwipeVC: UIViewController {
 
+    @IBOutlet weak var cardContainerView: UIView!
+    @IBOutlet weak var Card1: UIImageView!
+    @IBOutlet weak var Card2: UIImageView!
+    @IBOutlet weak var Card3: UIImageView!
+    
+    var originView: UIImageView?
+    var destinationView: UIImageView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        cardContainerView.addSubview(Card1)
+        cardContainerView.addSubview(Card2)
+        cardContainerView.addSubview(Card3)
+        view.addSubview(cardContainerView)
+        
+        print("originIndex = \(cardContainerView.subviews.startIndex)")
+        print("cardContainerView.subviews.count = \(cardContainerView.subviews.count)")
+        originView = cardContainerView.subviews[view.subviews.endIndex-1] as? UIImageView
 
         let swipe = UISwipeGestureRecognizer(target: self, action: #selector(swipeCard))
-        swipe.numberOfTouchesRequired = 2
+        swipe.numberOfTouchesRequired = 1
         swipe.direction = [.up, .down, .right, .left]
-        view.addGestureRecognizer(swipe)
+        cardContainerView.addGestureRecognizer(swipe)
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,8 +41,45 @@ class SwipeVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func prepareForNextSwipe() {
+        //originView = destinationView
+        destinationView = nil
+    }
+    
     func swipeCard(_ sender: UISwipeGestureRecognizer) {
         print("swipe gesture detected")
+        let transition = UIViewAnimationOptions.curveEaseOut
+        var indexOfDestinationView: Int?
+        let indexOfOriginView = cardContainerView.subviews.index(of: originView!)
+        
+        if !cardContainerView.subviews.isEmpty {
+            if indexOfOriginView != nil {
+                indexOfDestinationView = indexOfOriginView!-1
+            } else {
+                indexOfDestinationView = cardContainerView.subviews.startIndex
+            }
+                
+            if indexOfDestinationView! >= cardContainerView.subviews.count {
+                indexOfDestinationView = cardContainerView.subviews.startIndex
+            }
+            
+            if indexOfDestinationView! < cardContainerView.subviews.startIndex {
+                indexOfDestinationView = cardContainerView.subviews.count-1
+            }
+                
+            destinationView = cardContainerView.subviews[indexOfDestinationView!] as? UIImageView
+            
+            print("origin index = \(String(describing: indexOfOriginView!)), destination index = \(String(describing: indexOfDestinationView!))")
+            
+            UIView.transition(from: originView!, to: destinationView!, duration: 1.0, options: transition, completion: { finised in
+                self.originView = self.destinationView
+                self.prepareForNextSwipe()
+                
+            })
+        } else {
+            print("cardContainerView.subviews array is empty by accident")
+        }
+        
     }
 
     /*
